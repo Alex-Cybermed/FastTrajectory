f9715e64c854dd8de8ebcea14cb51f71c732d32f
Ying <ying.qiao@rutgers.edu>
2019-02-19 22:09:40 -0500

    initial and blocked random

--- FTRproject/src/application/GridNode.java
+++ FTRproject/src/application/GridNode.java
@@ -1,7 +1,7 @@
 package application;
 
 public class GridNode {
-	
+
 	private int X;
 	private int Y;
 	private char Status;
@@ -11,38 +11,55 @@ public class GridNode {
 	private int g;
 	private int f;
 	private int h;
-	
-	
+
+	public GridNode(int X, int Y, boolean visited, boolean blocked) {
+		this.X = X;
+		this.Y = Y;
+		this.visited = false;
+		this.blocked = false;
+		this.parent = null;
+	}
+
+
+
 	public int getX() {
 		return X;
 	}
+
 	public void setX(int x) {
 		X = x;
 	}
+
 	public int getY() {
 		return Y;
 	}
+
 	public void setY(int y) {
 		Y = y;
 	}
+
 	public boolean isVisited() {
 		return visited;
 	}
+
 	public void setVisited(boolean visited) {
 		this.visited = visited;
 	}
+
 	public boolean isBlocked() {
 		return blocked;
 	}
+
 	public void setBlocked(boolean blocked) {
 		this.blocked = blocked;
 	}
+
 	public GridNode getParent() {
 		return parent;
 	}
+
 	public void setParent(GridNode parent) {
 		this.parent = parent;
 	}
-	
 
 }
--- FTRproject/src/application/board.java
+++ FTRproject/src/application/board.java
@@ -1,43 +1,55 @@
 package application;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class board {
 	public static void main(String[] args) {
+		GridNode[][] board = initial();
+		printGridPane(board);
 		
 	}
-	public void inital( ) {
-		int[][] board = new int[100][100];
-		for (int i = 0; i < 100; i++) {
-			for (int j = 0; j < 100; j++) {
-		GridNode n = new GridNode();
+
+	public static GridNode[][] initial() {
+		int x = 5;
+		GridNode[][] board = new GridNode[x][x];
+		for (int i = 0; i < x; i++) {
+			for (int j = 0; j < x; j++) {
+				GridNode n = new GridNode(i, j, false, false);
+				//random blocked 30% gridNode
+				if(Math.random()<0.3) {
+					n.setBlocked(true);
+				}
+				board[i][j] = n;
+				
 			}
 		}
+		return board;	
 	}
-	
-	
-/*	public static int[][] initalizeBoard() {
-		int[][] board = new int[5][5];
-
-		return board;
-
-	}
-
-	public static void printBoard(int[][]board) {
-		for (int i = 0; i < 5; i++) {
-			for (int j = 0; j < 5; j++) {
-			
-				System.out.printf("%d%d", i, j);
+	public static void printGridPane(GridNode[][] board ) {
+		for(int i=0;i<board.length;i++) {
+			for(int j=0;j<board.length;j++) {
+				System.out.printf("%d%d%s", board[i][j].getX(), board[i][j].getY(),board[i][j].isBlocked());
 				System.out.print(" ");
+				
 			}
 			System.out.println();
 		}
 		System.out.println();
 	}
+	
 
-	public static void main(String[] args) {
-		int[][] board = initalizeBoard();
-		printBoard(board);
-	}*/
+	/*
+	 * public static int[][] initalizeBoard() { int[][] board = new int[5][5];
+	 * 
+	 * return board;
+	 * 
+	 * }
+	 * 
+	 * public static void printBoard(int[][]board) { for (int i = 0; i < 5; i++) {
+	 * for (int j = 0; j < 5; j++) {
+	 * 
+	 * System.out.printf("%d%d", i, j); System.out.print(" "); }
+	 * System.out.println(); } System.out.println(); }
+	 * 
+	 * public static void main(String[] args) { int[][] board = initalizeBoard();
+	 * printBoard(board); }
+	 */
 }
